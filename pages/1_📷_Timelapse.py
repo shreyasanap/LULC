f5892aec60a039548c0e013fc86d33c015a387b6
Laxmisneha chilukuri <laxmisnehac@gmail.com>
2024-09-12 22:49:40 +0530

    markdown in timelapse

--- pages/1_📷_Timelapse.py
+++ pages/1_📷_Timelapse.py
@@ -115,6 +115,13 @@ def app():
 
     st.title("Create Satellite Timelapse")
 
+    st.markdown(
+        """
+        An interactive web app for creating [Landsat](https://developers.google.com/earth-engine/datasets/catalog/landsat)/[GOES](https://jstnbraaten.medium.com/goes-in-earth-engine-53fbc8783c16) timelapse for any location around the globe.
+        The app was built using [streamlit](https://streamlit.io), [geemap](https://geemap.org), and [Google Earth Engine](https://earthengine.google.com). For more info, check out my streamlit [blog post](https://blog.streamlit.io/creating-satellite-timelapse-with-streamlit-and-earth-engine).
+    """
+    )
+
     row1_col1, row1_col2 = st.columns([2, 1])
 
     if st.session_state.get("zoom_level") is None:
